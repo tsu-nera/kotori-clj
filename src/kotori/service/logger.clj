4c14b93ca4768ffdf60e22fcf9dc242a7c7a9118
tsu-nera <fox10225fox@gmail.com>
2022-05-03 11:36:17 +0900

    :wrench: timbre設定見直し

---
+++ src/kotori/service/logger.clj
@@ -0,0 +1,28 @@
+(ns kotori.service.logger
+  (:require
+   [integrant.core :as ig]
+   [kotori.lib.time :as time]
+   [taoensso.timbre :as timbre]))
+
+(def timbre-config
+  {:timestamp-opts {:pattern  time/format-log
+                    :locale   time/locale-jst
+                    :timezone time/tz-jst}})
+
+(defn reset-config []
+  (timbre/set-config! {})
+  (timbre/set-config! timbre/default-config))
+
+(defmethod ig/init-key ::logger [_ _]
+  (reset-config)
+  (timbre/merge-config! timbre-config))
+
+(comment
+  (timbre/refer-timbre)
+  ;; (timbre/merge-config! timbre-config)
+
+  (timbre/info "test")
+  (timbre/debug "test")
+  (timbre/error "test")
+  timbre/*config*
+  )

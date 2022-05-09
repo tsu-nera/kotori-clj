aab21f5e37acb5f73329f3c4657587fdefd9b87a
tsu-nera <fox10225fox@gmail.com>
2022-05-09 19:34:35 +0900

    :sparkles: twitter-text-java libraryのClojure組込み
    ツイート文字数カウント.

---
+++ src/kotori/lib/twittertext.clj
@@ -0,0 +1,42 @@
+(ns kotori.lib.twittertext
+  (:import
+   (com.twitter.twittertext
+    TwitterTextParser)))
+
+(defn- parse-tweet [text]
+  (TwitterTextParser/parseTweet text))
+
+(defn parse [text]
+  (let [ret           (parse-tweet text)
+        length        (.weightedLength ret)
+        permillage    (.permillage ret)
+        valid?        (.isValid ret)
+        display-range (.displayTextRange ret)
+        valid-range   (.validTextRange ret)]
+    {:weighted-length     length
+     :permillage          permillage
+     :valid?              valid?
+     :display-range-start (.start display-range)
+     :display-range-end   (.end display-range)
+     :valid-range-start   (.start valid-range)
+     :valid-range-end     (.end valid-range)}))
+
+(defn get-length "
+  全角では140文字, 半角だと280文字.
+  混在の場合のルールがよくわからないので結局ライブラリで計算がいい."
+  [text]
+  (let [ret   (parse text)
+        start (:valid-range-start ret)
+        end   (:valid-range-end ret)]
+    (- end start)))
+
+(comment
+  ;;
+  (def sample1 "test")
+  (def sample2 "【配信限定特典映像付き】朝ドラ系現役アイドルT○kT○ker 西元めいさ 初体験で初絶頂 ＃初イキ ＃初巨根 ＃初3P ＃キャパオーバー ＃快感 ＃くびれボディ ＃ビクッビク【圧倒的4K映像でヌク！】")
+
+  (parse sample2)
+  (get-length sample2)
+
+  ;;
+  )
